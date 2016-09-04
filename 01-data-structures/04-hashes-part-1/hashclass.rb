a5bc2ac939bf83b159224f37bea44f14099b5398
Tracy Royal <TracyRoyal@mac.com>
2016-09-04 14:03:49 -0400

    more work on hashes

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -6,15 +6,53 @@ class HashClass
 
   #add entry into hash
   def []=(key, value)
-    #hash_index = index(key, @items)
-    #new_hash_item =
+    movie_index = index(key, @items.size)
+    if @items[movie_index].nil?
+      @items[movie_index] = HashItem.new(key, value)
+    elsif @items[movie_index].key == key
+      unless @items[movie_index].value == value
+        @items[movie_index].value = value
+        self.resize
+      end
+    else
+      self.resize
+      self[key] = value
+    end
+
   end
 
-
   def [](key)
+    movie_index = index(key, @items.size)
+      if @items[movie_index] == nil
+        return "error"
+      else
+        @items[movie_index].value
+      end
+
   end
 
   def resize
+    #create a new array
+    double_array = Array.new(@items.size)
+    #create an arrayto hold items
+    expanded_array = []
+    @items.each do |movie|
+      unless @item == nil
+        expanded_array << movie
+      end
+    end
+    #wipe out the "too small"array
+    #map! mutates the original array
+    @items.map! do |array_item|
+      unless array_item == nil
+        array_item = nil
+      end
+    end
+    #found concat after trying toString, join
+    @items.concat(double_array)
+      expanded_array.each do |movie|
+      self[item.key] = item.value
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
@@ -22,7 +60,7 @@ class HashClass
   # a starting point.
   def index(key, size)
     ascii = 0
-    key.each_ do |x|
+    key.each_byte do |x|
       ascii += x
     end
     puts ascii
