0e6f7a5f7d6d73a97654780ef028776e7e376963
Tracy Royal <troyal@tss.tssgis.com>
2016-11-03 11:34:36 -0400

    first pass on trees

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -7,27 +7,81 @@ class BinarySearchTree
   end
 
   def insert(root, node)
-    if root == nil
-      root = node
-    elsif node.rating < root.rating && root.left == nil
-      root.left = nil
-    elsif node.rating > root.rating && root.right == nil
-      root.right = nil
-    elsif node.rating < root.rating
-      insert(root.left, node)
-    elsif node.rating > root.rating
-      insert(root.right, node)
+    if root != nil
+      if root.movie_rating > node.movie_rating
+        if root.left == nil
+          root.left = node
+        else
+          self.insert(root.left, node)
+        end
+      elsif root.movie_rating < node.movie_rating
+        if root.right == nil
+          root.right = node
+        else
+          self.insert(root.right, node)
+        end
+      end
     end
   end
 
   # Recursive Depth First Search
-  def find(root, data)
+  #travel down single branch in search of desired element, if not found, travel up and
+  #repeat the process with unvisited nodes
+  def find(root, movie_rating)
+    if movie_rating != nil && root != nil
+      if root.movie_rating == movie_rating
+        return root
+      elsif root.movie_rating > movie_rating
+        if root.left != nil
+          self.find(root.left, movie_rating)
+        end
+      elsif root.movie_rating < movie_rating
+        if root.left != nil
+          self.find(root.right, movie_rating)
+        end
+      end
+    end
   end
 
-  def delete(root, data)
+  def delete(root, movie_rating)
+    if root == nil
+      return nil
+    elsif movie_rating < root.movie_rating
+      root.left = self.delete(root.left, movie_rating)
+    elsif movie_rating > root.movie_rating
+      root.right = self.delete(root.right, movie_rating)
+    else
+      if root.left == nil && root.right == nil
+        return nil
+      elsif root.right == nil
+        return root.left
+      elsif root.left == nil
+        return root.right
+      end
+    end
   end
 
   # Recursive Breadth First Search
+  #search an entire row of nodes before proceeding to the next
   def printf(children=nil)
+    if children = nil
+      puts @root.title + ": " + @root.movie_rating.to_s
+      children = [@root.left, @root.right]
+      printf[children]
+    else
+      new_children = []
+      children.each do |new_children|
+      puts new_children.title + ": " + new_children.movie_rating.to_s
+      unless new_children.left.nil?
+        new_children << new_children.left
+      end
+      unless new_children.right.nil?
+        new_children << new_children.right
+      end
+      unless new_children.empty?
+        printf(new_children)
+      end
+    end
   end
+end
 end
