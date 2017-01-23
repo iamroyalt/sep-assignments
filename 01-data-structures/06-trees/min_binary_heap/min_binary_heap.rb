97d8f83576478c73e8fceb8c0f2496716789b168
Tracy Royal <troyal@tss.tssgis.com>
2017-01-23 08:56:39 -0500

    heap passing not binary

--- 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
+++ 01-data-structures/06-trees/min_binary_heap/min_binary_heap.rb
@@ -1,20 +1,68 @@
 require_relative 'node'
 
-class BinarySearchHeap
-  attr_reader :heap
+class MinBinaryHeap
+  attr_reader :root
 
-def initialize
-  @heap = root
+  def initialize(root)
+    @root = root
+  end
+
+def insert(root, node)
+
+  if root.rating > node.rating
+    temp = root
+    @root = node
+    node = temp
+    insert(@root, node)
+  else
+    if root.left.nil?
+      root.left = node
+    elsif root.right.nil? && root.left != nil
+      root.right = node
+    elsif root.left != nil && root.right != nil && root.left.left != nil && root.left.right != nil
+      insert(root.right, node)
+    elsif root.left != nil && root.right != nil
+      insert(root.left, node)
+    end
+  end
 end
 
-def insert(root, data)
-end
+  def find(root, data)
+    if root.nil? || data.nil?
+      return nil
+    else
+      if root.title == data
+        return root
+      elsif root.left != nil
+        find(root.left, data)
+      elsif root.right != nil
+        find(root.right, data)
+      end
+    end
+  end
 
-def delete(root, data)
-end
+  def delete(root, data)
+    if root.nil? || data.nil?
+      return nil
+    else
+      target_node = find(root, data)
+      target_node.nil? ? nil : (target_node.title = nil && target_node.rating = nil)
+    end
+  end
 
-def find(root, data)
-end
-
-def print(root)
+  def printf
+    queue = [@root]
+    result = []
+    while queue.length > 0
+      new_root = queue.shift
+      if new_root.left != nil
+        queue.push(new_root.left)
+      end
+      if new_root.right != nil
+        queue.push(new_root.right)
+      end
+      result.push("#{new_root.title}: #{new_root.rating}")
+    end
+    result.each {|x| puts x}
+  end
 end
